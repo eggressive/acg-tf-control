a9d1099525dabcf1073c04f9df6880cd45526e7e
Dimitar Dimitrov <mitko.69@gmail.com>
2022-08-31 01:33:12 +0200

    removed credentials in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAYTUGZTJL2CKOKJ5F"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
