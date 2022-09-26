e142455e1b6d93c77cb4745411ac45aef0943dda
Dimitar <47664474+eggressive@users.noreply.github.com>
2022-09-26 21:27:56 +0200

    User data enhancements

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -ex
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo yum clean all
 sudo yum -y update
 sudo yum install -y yum-utils git
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+terraform version > /var/tmp/tf-version.txt
+su - ec2-user -c 'terraform -install-autocomplete'
